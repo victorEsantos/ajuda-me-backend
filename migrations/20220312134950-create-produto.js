5c847619b807dd326d33ef8dc11cdccbcccadda5
victor <victor.eduardo.sntos@gmail.com>
2022-03-12 11:59:50 -0300

    init repo

---
+++ migrations/20220312134950-create-produto.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Produtos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      quantidade: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Produtos');
+  }
+};
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Para iniciar a aplicação digite:
+## yarn dev
+
+# para subir um container do postgres no docker:
+## docker run --name my-postgres -e "POSTGRES_PASSWORD=pac123" -p 5432:5432 -d postgres
+
+
+# Para criar um model (mapeamento de uma entidade do banco no codigo):
+## yarn sequelize-cli model:generate --name NomeEntidade --attributes nome:string,quantidade:integer
+
+
+# Comandos do migration do banco
+##  Para criar o banco definido no arquivo config.json:
+### yarn sequelize-cli db:create
+## Para rodar o migrate e criar as entidades anteriormente geradas pelo "sequelize-cli model:generate":
+### yarn sequelize-cli db:migrate
